2a322a76de4b0680e859efee2bbf3294ce25c34f
gustavo.silva <gustavo.silva@codificar.com.br>
2020-05-16 10:12:17 -0300

    Import Maps

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import * as VueGoogleMaps from "vue2-google-maps";
+
 Vue.config.productionTip = false;
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyBo7bGaEkoB45d5TQzelkIu39a23Em4HSM",
+    libraries: "places"
+  }
+});
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <google-map />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import GoogleMap from "@/components/GoogleMap.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    GoogleMap
   }
 };
 </script>
